e132714af85335399fd1dd4f11dcabf215d5c98a
ossan <ossan.dry.tomato@gmail.com>
2019-03-09 22:36:27 +0900

    add メモをリストに追加する機能

--- src/FormInput.elm
+++ src/FormInput.elm
@@ -20,12 +20,16 @@ main =
 
 
 type alias Model =
-    {}
+    { input : String
+    , memos : List String
+    }
 
 
 init : Model
 init =
-    {}
+    { input = ""
+    , memos = []
+    }
 
 
 
@@ -33,14 +37,38 @@ init =
 
 
 type Msg
-    = Msg
+    = Input String
+    | Submit
 
 
 update : Msg -> Model -> Model
 update msg model =
-    model
+    case msg of
+        -- 入力文字列を更新する
+        Input input ->
+            { model | input = input }
+
+        -- 入力文字列をリセットする＋最新のメモを追加する
+        Submit ->
+            { model
+                | input = ""
+                , memos = model.input :: model.memos
+            }
 
 
 view : Model -> Html Msg
 view model =
-    text ""
+    div []
+        [ Html.form [ onSubmit Submit ]
+            [ input [ value model.input, onInput Input ] []
+            , button
+                [ disabled (String.isEmpty <| String.trim model.input) ]
+                [ text "Submit" ]
+            , ul [] (List.map viewMemo model.memos)
+            ]
+        ]
+
+
+viewMemo : String -> Html Msg
+viewMemo memo =
+    li [] [ text memo ]
