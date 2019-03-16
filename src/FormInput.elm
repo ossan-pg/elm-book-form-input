427f999a30b08beca1cc72944eb6fe663d838b81
ossan <ossan.dry.tomato@gmail.com>
2019-03-16 19:32:50 +0900

    fix: メモ削除後に表示上のチェック状態を解除されるようにした

--- src/FormInput.elm
+++ src/FormInput.elm
@@ -64,18 +64,19 @@ update msg model =
         Check index checked ->
             if checked then
                 { model | checked = Set.insert index model.checked }
+
             else
                 { model | checked = Set.remove index model.checked }
 
         -- チェックされたメモを削除する
         Delete ->
             let
-                newMemos = List.indexedMap Tuple.pair model.memos
-                    |> List.filter (\pair -> Set.member (Tuple.first pair) model.checked |> not)
-                    |> List.map Tuple.second
+                newMemos =
+                    List.indexedMap Tuple.pair model.memos
+                        |> List.filter (\pair -> Set.member (Tuple.first pair) model.checked |> not)
+                        |> List.map Tuple.second
             in
             { model | memos = newMemos, checked = Set.empty }
-
 
 
 view : Model -> Html Msg
@@ -90,10 +91,19 @@ view model =
         , button
             [ disabled <| Set.isEmpty model.checked, onClick Delete ]
             [ text "Delete" ]
-        , ul [] (List.indexedMap viewMemo model.memos)
+        , ul [] (viewMemo model)
         ]
 
 
-viewMemo : Int -> String -> Html Msg
-viewMemo index memo =
-    li [] [ input [ type_ "checkbox", onCheck <| Check index ] [] , text memo ]
+viewMemo : Model -> List (Html Msg)
+viewMemo model =
+    let
+        checkedMemos =
+            List.indexedMap (\index memo -> ( Set.member index model.checked, memo )) model.memos
+    in
+    List.indexedMap viewMemoHelp checkedMemos
+
+
+viewMemoHelp : Int -> ( Bool, String ) -> Html Msg
+viewMemoHelp index ( checked_, memo ) =
+    li [] [ input [ type_ "checkbox", onCheck <| Check index, checked checked_ ] [], text memo ]
